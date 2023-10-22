b6f47162f0289848e85f21a32fb27fb8743e55d2
tk-roh <127922089+tk-roh@users.noreply.github.com>
2023-10-22 17:03:31 +0900

    calc.jsを編集しました。

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -2,14 +2,14 @@
 let num = Math.floor(Math.random() * 51);
 
 //変数numによって、出力する文字列を切り替える
-if (num % 3 === 0) {
-    console.log('3の倍数です');
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
 }
 else if (num % 5 === 0) {
     console.log('5の倍数です');
 }
-else if (num % 3 === 0 && num % 5 === 0) {
-    console.log('3と5の倍数です');
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
 }
 else {
     console.log(num);
