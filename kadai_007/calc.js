60aa94097ec1aeef8f0edadf897d17bd1bc9ab2f
tk-roh <127922089+tk-roh@users.noreply.github.com>
2023-10-22 16:39:33 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+//変数numに0～50までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 51);
+
+//変数numによって、出力する文字列を切り替える
+if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+else if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+else if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+else {
+    console.log(num);
+}
